d33452d3f6901bf403120010d1cf81ef55363536
Sergey Trusov <serezha.molodec@gmail.com>
2018-05-30 13:41:44 +0300

    21

--- src/main.go
+++ src/main.go
@@ -2,9 +2,39 @@
 // Click here and start typing.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox"
+	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox/files"
+)
 
 func main() {
 
-	fmt.Println("Hello, vxc")
+	config := dropbox.Config{
+		Token:    "C8mCXzvXFNAAAAAAAAABaQ9C82-fhkyosKdWMZR9t3ohpPrzu3OtUmjWcKd8w-qk",
+		LogLevel: dropbox.LogInfo, // if needed, set the desired logging level. Default is off
+	}
+	/* 	dbx := users.New(config) */
+
+	lfob := files.NewListFolderArg("/приложения/WahooFitness")
+
+	filesdb := files.New(config)
+
+	lf, err := filesdb.ListFolder(lfob)
+
+	if err != nil {
+		return
+	}
+
+	fmt.Printf(strconv.FormatBool(lf.HasMore))
+
+	return
+	/*arg := users.NewGetAccountArg(accountId)
+	if resp, err := dbx.GetAccount(arg); err != nil {
+		return err
+	}
+	fmt.Printf("Name: %v", resp.Name)
+	*/
 }
